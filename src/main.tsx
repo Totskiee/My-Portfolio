97c5c3da51cad5f61444b4ce8154bb0989434b7a
Totskiee <jerneiljao@gmail.com>
2026-08-13 13:43:45 +0800

    progress

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,26 @@
 import "@/styles/global.css";
-
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/home";
-
-
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+import HomePage from "@/pages/home";
+import AboutPage from "@/pages/about";
+import ProjectsPage from "@/pages/projects";
+import ContactPage from "@/pages/contact";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      {/* Guest Pages */}
+  <StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
         <Route path="/" element={<HomePage />} />
-    
-
-      {/* Dashboard Pages */}
-    </Routes>
-  </BrowserRouter>,
-)
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </StrictMode>
+);
